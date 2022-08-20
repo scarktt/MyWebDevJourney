3e1acf2710f410400dfbf27822513e43714c6cd9
ssoteloserrano <ssoteloserrano@gmail.com>
2022-08-20 00:38:55 -0600

    Add Country and Follower class

--- backend/anime_center_project/apps/users/models.py
+++ backend/anime_center_project/apps/users/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
+from datetime import date
+
+
+class Country(models.Model):
+    country_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+
+
 class User(models.Model):
-    id = models.CharField(max_length=25, primary_key=True)
+    user_id = models.CharField(max_length=25, primary_key=True)
     display_name = models.CharField(max_length=255, unique=True)
+    birth_date = models.DateField(default=date.today, blank=True)
     email = models.EmailField(max_length=254)
-    country = models.CharField(max_length=90, blank=True, null=True)
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, blank=True, null=True)
     external_urls = models.JSONField("ExternalUrls", default=dict, blank=True)
-    followers = models.IntegerField()
     images = models.JSONField("Images", default=dict)
 
 
+class Follower(models.Model):
+    follower_id = models.IntegerField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    begin_date = models.DateTimeField()
