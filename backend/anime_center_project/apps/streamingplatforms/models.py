08eb1f34d1e7e5d4e0858b00ae043effd138e74c
ssoteloserrano <ssoteloserrano@gmail.com>
2022-08-22 22:38:17 -0600

    Add Streaming Platforms models

--- backend/anime_center_project/apps/streamingplatforms/models.py
+++ backend/anime_center_project/apps/streamingplatforms/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from .. animes.models import Anime
+
+
+class StreamingPlatform(models.Model):
+    streamingplatform_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    urls = models.CharField(max_length=255)
+
+
+class AnimeStreamingPlatform(models.Model):
+    anime_id = models.ForeignKey(Anime, on_delete=models.CASCADE)
+    streamingplatform_id = models.ForeignKey(StreamingPlatform, on_delete=models.CASCADE)
+    anime_url = models.CharField(max_length=255)
+    is_available = models.BooleanField(default=True)
