9b2aa19a5afd7e9d36d6c791fdcd49a3bab3b911
ssoteloserrano <ssoteloserrano@gmail.com>
2022-08-23 21:07:49 -0600

    Add List models

--- backend/anime_center_project/apps/lists/models.py
+++ backend/anime_center_project/apps/lists/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from .. users.models import User
+
+
+class List(models.Model):
+    list_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    is_private = models.BooleanField(default=False)
+
+
+class UserList(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    list_id = models.ForeignKey(List, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
