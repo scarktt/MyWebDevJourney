23bd3629fadb366d86ade3cbfabf677799ecc7df
ssoteloserrano <ssoteloserrano@gmail.com>
2022-08-24 23:32:40 -0600

    Remove animelist model

--- backend/anime_center_project/apps/animes/models.py
+++ backend/anime_center_project/apps/animes/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 from .. genres.models import Genre
-from .. users.models import User
-from .. lists.models import List
 
 
 class Broadcast(models.Model):
@@ -28,36 +26,6 @@ class Anime(models.Model):
     season_id = models.ForeignKey(Season, on_delete=models.SET_NULL, blank=True, null=True)
     broadcast_id = models.ForeignKey(Broadcast, on_delete=models.SET_NULL, blank=True, null=True)
     source = models.CharField(max_length=255)
-
-
-class AnimeGenres(models.Model):
-    anime_id = models.ForeignKey(Anime, on_delete=models.SET_NULL, blank=True, null=True)
-    genre_id = models.ForeignKey(Genre, on_delete=models.SET_NULL, blank=True, null=True)
-
-
-class UserAnime(models.Model):
-
-    class States(models.TextChoices):
-        WATCHING = 'watching', 'Watching'
-        PAUSED = 'paused', 'Paused'
-        FINISHED = 'finished', 'Finished'
-
-    class Scores(models.IntegerChoices):
-        one = 1, '1'
-        two = 2, '2'
-        three = 3, '3'
-        four = 4, '4'
-        five = 5, '5'
-
-    useranime_id = models.IntegerField(primary_key=True)
-    anime_id = models.ForeignKey(Anime, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    state = models.CharField(max_length=8, choices=States.choices)
-    score = models.IntegerField(choices=Scores.choices)
-
-class AnimeList(models.Model):
-    anime_id = models.ForeignKey(Anime, on_delete=models.CASCADE)
-    list_id = models.ForeignKey(List, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    genre = models.ManyToManyField(Genre)
 
 
