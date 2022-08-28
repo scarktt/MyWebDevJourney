1266bb823eb1c179d581fa37d4b56aa560383e54
ssoteloserrano <ssoteloserrano@gmail.com>
2022-08-27 21:52:56 -0600

    Add meta class and dunder string method to users model

--- backend/anime_center_project/apps/users/models.py
+++ backend/anime_center_project/apps/users/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 from datetime import date
 
+from .. animes.models import Anime
+from .. lists.models import List
+
 
 class Country(models.Model):
     country_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
+
+
+    def __str__(self):
+        return self.name
 
 
 class User(models.Model):
@@ -15,11 +22,68 @@ class User(models.Model):
     email = models.EmailField(max_length=254)
     country = models.ForeignKey(Country, on_delete=models.SET_NULL, blank=True, null=True)
     external_urls = models.JSONField("ExternalUrls", default=dict, blank=True)
-    images = models.JSONField("Images", default=dict)
+    images = models.JSONField("Images", default=dict, blank=True)
+    anime = models.ManyToManyField(Anime,
+        through='UserAnime',
+        related_name='user_id'
+    )
+    lists = models.ManyToManyField(List,
+        through='UserList',
+        related_name='user_id'
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Follower(models.Model):
-    follower_id = models.IntegerField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['user_id', 'created_at']
+
+
+    def __str__(self):
+        return self.display_name
+
+
+class UserFollowing(models.Model):
+    user_id = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
+    following_user_id = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
+    is_unfollowed = models.BooleanField(default=False)
     begin_date = models.DateTimeField()
+
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user_id','following_user_id'],  name="unique_followers")
+        ]
+
+        ordering = ["-begin_date"]
+
+
+    def __str__(self):
+        f"{self.user_id} follows {self.following_user_id}"
+
+
+class UserAnime(models.Model):
+
+    class States(models.TextChoices):
+        WATCHING = 'watching', 'Watching'
+        PAUSED = 'paused', 'Paused'
+        FINISHED = 'finished', 'Finished'
+
+    class Scores(models.IntegerChoices):
+        one = 1, '1'
+        two = 2, '2'
+        three = 3, '3'
+        four = 4, '4'
+        five = 5, '5'
+
+    anime_id = models.ForeignKey(Anime, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    state = models.CharField(max_length=8, choices=States.choices)
+    score = models.IntegerField(choices=Scores.choices)
+
+
+class UserList(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    list_id = models.ForeignKey(List, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
