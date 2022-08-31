7af4d3bd445a92d4b66c859057812c9c017f7575
ssoteloserrano <ssoteloserrano@gmail.com>
2022-08-30 21:21:09 -0600

    Remove many to many class with anime

--- backend/anime_center_project/apps/streamingplatforms/models.py
+++ backend/anime_center_project/apps/streamingplatforms/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from .. animes.models import Anime
-
 
 class StreamingPlatform(models.Model):
     streamingplatform_id = models.IntegerField(primary_key=True)
@@ -9,8 +7,7 @@ class StreamingPlatform(models.Model):
     urls = models.CharField(max_length=255)
 
 
-class AnimeStreamingPlatform(models.Model):
-    anime_id = models.ForeignKey(Anime, on_delete=models.CASCADE)
-    streamingplatform_id = models.ForeignKey(StreamingPlatform, on_delete=models.CASCADE)
-    anime_url = models.CharField(max_length=255)
-    is_available = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
+
+
