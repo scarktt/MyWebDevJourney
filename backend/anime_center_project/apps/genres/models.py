e09cb86419fa698830e5fd148854fc2684a8fdbc
ssoteloserrano <ssoteloserrano@gmail.com>
2022-08-30 21:18:44 -0600

    Add dunder string method to genre model

--- backend/anime_center_project/apps/genres/models.py
+++ backend/anime_center_project/apps/genres/models.py
@@ -5,3 +5,7 @@ class Genre(models.Model):
     genre_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
+    
+
+    def __str__(self):
+        return self.name
